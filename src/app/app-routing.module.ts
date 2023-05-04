299fd4eb3920fc5705fcba7c79d5dcc480f573b0
DavidZomber935 <davidbra12345@gmail.com>
2023-05-04 15:54:38 -0500

    Deber

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AcercaComponent } from './acerca/acerca.component';
+import { VisitanosComponent } from './visitanos/visitanos.component';
+import { Err404Component } from './err404/err404.component';
+import { RedesContactoComponent } from './redes-contacto/redes-contacto.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path: 'home',component:HomeComponent},
+  {path: 'acerca',component:AcercaComponent},
+  {path: 'visitanos',component:VisitanosComponent},
+  {path: 'redes-contacto', component: RedesContactoComponent},
+  {path: '', redirectTo:'home', pathMatch:'full'},
+  {path: '**', component:Err404Component}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { AcercaComponent } from './acerca/acerca.component';
+import { VisitanosComponent } from './visitanos/visitanos.component';
+import { Err404Component } from './err404/err404.component';
+import { RedesContactoComponent } from './redes-contacto/redes-contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    AcercaComponent,
+    VisitanosComponent,
+    Err404Component,
+    RedesContactoComponent
   ],
   imports: [
     BrowserModule,
